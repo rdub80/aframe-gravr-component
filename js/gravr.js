01ff5220a8a19b8454414ed8baffb1c42ee19b2c
Roland Dubois <rdub80@gmail.com>
2016-12-11 14:47:22 -0500

    button vr yes no

--- js/gravr.js
+++ js/gravr.js
@@ -37,22 +37,29 @@ function getCookie(c_name) {
 	var usrBoundRight = 180; // default 
 	var usrBoundBottom = 180; // default 
 	var usrBoundLeft = 180; // default 
+	var usrContrast = 0; // default
+	var usrMagnify = 0; // default
 	var clickDelay = 1000; // default (ms)
 
 
-// get cookie values if cookie already exists if not prompt setup
+// get cookie values if cookie already exists
 
 if( getCookie(c_CamHeight) != "" ){
+	
 	getCookie(c_CamHeight);
 	getCookie(c_CamFov);
 	getCookie(c_UsrBoundTop);
 	getCookie(c_UsrBoundRight);
 	getCookie(c_UsrBoundBottom);
 	getCookie(c_UsrBoundLeft);
+	getCookie(c_UsrContrast);
+	getCookie(c_UsrMagnify);
 	getCookie(c_ClickDelay);
+
 } else {
 
-	// here we can connect with the service to get updated data on gravr.io API with user email
+	// here we prompt setup of values
+	// later we can connect with the service to get updated data on gravr.io API with user email
 
 	camHeight = 1.8;
 	camFov = 90;
@@ -60,6 +67,8 @@ if( getCookie(c_CamHeight) != "" ){
 	usrBoundRight = 180;
 	usrBoundBottom = 180;
 	usrBoundLeft = 180;
+	usrContrast = 0.5;
+	usrMagnify = 0.5;
 	clickDelay = 1000;
 
 	createCookie(c_CamHeight, camHeight);
@@ -68,6 +77,8 @@ if( getCookie(c_CamHeight) != "" ){
 	createCookie(c_UsrBoundRight, usrBoundRight);
 	createCookie(c_UsrBoundBottom, usrBoundBottom);
 	createCookie(c_UsrBoundLeft, usrBoundLeft);
+	createCookie(c_UsrContrast, usrContrast);
+	createCookie(c_UsrMagnify, usrMagnify);
 	createCookie(c_ClickDelay, clickDelay);
 
 }
@@ -81,6 +92,8 @@ AFRAME.registerComponent('gravr', {
    var userheight = camHeight;
    var fov = camFov;
    var UIBounds = [-0, 0, 0, -0]; //(top, right, left, bottom)
+   var UIContrast = 0; 
+   var UIMagnify = 0; 
    var gazeDelay = 1000; //(ms)
    var object3D = this.el.object3D;
    var data = this.data;
@@ -91,7 +104,21 @@ AFRAME.registerComponent('gravr', {
 
 // create profile menu
 
-// add button on center of view
-// make button visible on shake
+// now 
+// 1. read cookie data and button
+// 2. make button clickable and associate trigger function to apply gravr values
+
+// later 
+// 1. read json feed from gravr profile
+// 2. iterate through profile sets building buttons for each preset array
+
+
+// activate profile menu
+
+// 1. read current camera gaze x y z position
+// 2. place gravr profile button on center of view
+// 3. fade out is gaze control is not hovering
+// 4. make visible on shake - https://github.com/alexgibson/shake.js/
+// 5. refresh gaze xyz for new profile button positioning (like 1.)
 
 
